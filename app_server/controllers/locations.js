fc7690aa299d1f15f618f91bc6502dc45fbde1f1
deepak-karkala <dkarkala01@gmail.com>
2016-08-05 18:13:07 +0200

    Moved data from views to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,14 +1,49 @@
 /* GET 'home' page */
 module.exports.list = function(req, res){
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', { 
+  	title: 'Restaurants Near Me',
+  	pageHeader:{
+  		title: 'Restaurants Near Me',
+  		strapline: 'Find nearby restaurants'
+  	},
+  	sidebar: "Looking for a place to have a sumptuous meal ? Restaurants Near Me helps you locate the restaurants close to you.",
+  	locations: [{
+  		name: 'Starmeal',
+  		address: '123 First street, Neverland'	
+  	},{
+  		name: 'Meal Hero',
+  		address: '789 Third street, Yesterdayland'	
+  	},{
+  		name: 'Meal King',
+  		address: '456 Second street, Tomorrowland'	
+  	}] 
+  });
 };
 
 /* GET 'Location info' page */
 module.exports.detail = function(req, res){
-  res.render('location-detail', { title: 'Location detail' });
+  res.render('location-detail', { 
+  	title: 'Starmeal',
+  	pageHeader: {title: 'Starmeal'},
+  	location: {
+  		name: 'Starmeal',
+  		address: '123 First street, Neverland',
+  		coords: {lat: 46.541292, lng: 6.629719},
+  		reviews: [{
+  			author: 'Deepak Karkala',
+  			reviewText: 'Great place. I highly recommend it.'	
+  		}]
+  	}
+
+  });
 };
 
 /* GET 'Add review' page */
 module.exports.review = function(req, res){
-  res.render('location-review', { title: 'Add review' });
+  res.render('location-review', {
+   title: 'Add review',
+   pageHeader: { title: 'Review Starcups'} 
+  });
 };
+
+
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,6 @@
 module.exports.about = function(req,res) {
-  res.render('about', { title: 'About' });
+  res.render('about', { 
+  	title: 'About',
+  	content: 'Restaurants Near Me helps you to find restaurants in your nearby locations.'
+  });
 };
